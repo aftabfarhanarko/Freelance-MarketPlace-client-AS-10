d59df25e79bc80bf45ccee2943e806ac441c0550
Aftab Farhan <aftabfarhan324@gmail.com>
2025-11-09 06:16:57 +0600

    addes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,19 +5,20 @@ import {
   FaLinkedinIn,
   FaTwitter,
 } from "react-icons/fa6";
-import x from '../assets/x.png'
+import x from "../assets/x.png";
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-gray-300 py-10">
-      <div className="max-w-7xl mx-auto px-6 md:px-12">
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+      <div className="w-11/12 mx-auto px-6 md:px-12">
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-10">
           {/* About */}
           <div>
             <h3 className="text-xl font-semibold mb-4 text-white">About Us</h3>
             <p className="text-gray-400 leading-relaxed text-sm">
-              We are committed to providing the best services to our customers.
-              Our mission is to deliver high-quality products with exceptional
-              support.
+              Discover a world where talent meets opportunity. Our freelance
+              marketplace connects skilled professionals with clients seeking
+              quality work and creative solutions. Empowering freelancers to
+              grow, earn, and shine globally.
             </p>
           </div>
 
@@ -64,7 +65,40 @@ const Footer = () => {
               <li>Address: 1234 Street, City, Country</li>
             </ul>
           </div>
-
+          <div>
+            <div>
+              <h3 className="text-xl font-semibold mb-4 text-white">
+                Services
+              </h3>
+              <ul className="space-y-2 text-gray-400">
+                <li>
+                  <a href="#" className="hover:text-pink-500 transition">
+                    Branding
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-pink-500 transition">
+                    Design
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-pink-500 transition">
+                    Marketing
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-pink-500 transition">
+                    Advertisement
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-pink-500 transition">
+                    Blog
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
           {/* Social */}
           <div>
             <h3 className="text-xl font-semibold mb-4 text-white">Follow Us</h3>
@@ -81,7 +115,7 @@ const Footer = () => {
                 className="p-1.5 rounded-full bg-gray-800 hover:bg-pink-500 transition"
                 aria-label="Twitter"
               >
-             <img className="w-5 h-5 rounded-full" src={x}></img>
+                <img className="w-5 h-5 rounded-full" src={x}></img>
               </a>
               <a
                 href="#"
@@ -97,6 +131,14 @@ const Footer = () => {
               >
                 <FaLinkedinIn />
               </a>
+            </div>
+            <div className="join mt-4">
+              <input
+                type="text"
+                placeholder="username@site.com"
+                className="input input-bordered bg-gray-500 join-item"
+              />
+              <button className="btn btn-primary join-item">Search Now</button>
             </div>
           </div>
         </div>
